2867570da19e4a5d2611f6ac0c4eac4c5cce3310
WuJiewei <1790992954@qq.com>
2020-10-20 10:05:20 +0800

    建造者模式

---
+++ src/com/ikilig/pattern/prototype/improve/Client.java
@@ -0,0 +1,22 @@
+package com.ikilig.pattern.prototype.improve;
+
+public class Client {
+
+    public static void main(String[] args) {
+
+        System.out.println("使用了原型模式创建对象");
+        Sheep sheep = new Sheep("tom", 1, "白色");
+        Sheep sheep1 = (Sheep) sheep.clone();
+        Sheep sheep2 = (Sheep) sheep.clone();
+        Sheep sheep3 = (Sheep) sheep.clone();
+        Sheep sheep4 = (Sheep) sheep.clone();
+        Sheep sheep5 = (Sheep) sheep.clone();
+
+        System.out.println(sheep);
+        System.out.println(sheep1);
+        System.out.println(sheep2);
+        System.out.println(sheep3);
+        System.out.println(sheep4);
+        System.out.println(sheep5);
+    }
+}
